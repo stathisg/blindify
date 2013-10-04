1601e4461e6471eef548eb790805e1591a8e5e4a
stathisg <me@stathisg.com>
2013-10-04 17:39:44 +0100

    initial commit

---
+++ jquery.blindify.js
@@ -0,0 +1,165 @@
+/*!
+ * Blindify - jQuery Plugin - v0.1
+ * https://github.com/stathisg/blindify
+ *
+ * Copyright 2013 Stathis Goudoulakis
+ * Released under the MIT license
+ */
+ (function($){
+    images = new Array();
+    urls = new Array();
+    activeImage = 0;
+    blinds = null;
+    el = null;
+
+    defaultOptions = {
+        numberOfBlinds: 20,
+        slideVisibleTime: 2000,
+        color: '#000000',
+        margin: 2,
+        width: 960,
+        height: 600,
+        gap: 100,
+        animationSpeed: 800,
+        delayBetweenSlides: 500,
+        hasLinks: false,
+        orientation: 'vertical'
+    };
+
+    options = {};
+
+    $.blindify = function(element, userOptions) {
+        el = element;
+        var $el = $(el);
+
+        options = $.extend({}, defaultOptions, userOptions);
+
+        $('ul li', el).each(function() {
+            var tempEl = $(this);
+            if(options.hasLinks) {
+                urls.push(tempEl.children('a')[0].href);
+            }
+            images.push(tempEl);
+        });
+
+        images[0].addClass('active');
+
+        if(options.hasLinks) {
+            var masterA = document.createElement('a');
+            masterA.href = urls[0];
+            $el.prepend(masterA);
+            var movingElements = $('ul', el).detach();
+            $(masterA).append(movingElements);
+            el = masterA;
+            $el = $(el);
+        }
+
+        $el.addClass('blindify')
+           .width(options.width)
+           .height(options.height);
+        $('img', el).width(options.width)
+                    .height(options.height);
+
+        var spanWidth = 0;
+        var spanHeight = 0;
+
+        if(options.orientation === 'vertical') {
+            spanWidth = options.width / options.numberOfBlinds;
+        } else {
+            spanHeight = options.height / options.numberOfBlinds;
+        }
+
+        for (var i = 0; i < options.numberOfBlinds; i++) {
+            var tempEl = $(document.createElement('span'));
+            var borders = calculateBorders();
+
+            if(options.orientation === 'vertical') {
+                tempEl.css({
+                    'left': i * spanWidth,
+                    border: options.margin + 'px solid ' + options.color,
+                    borderTop: borders.borderWidthTop + 'px solid ' + options.color,
+                    borderBottom: borders.borderWidthBottom + 'px solid ' + options.color,
+                    'height': options.height,
+                    'width': spanWidth
+                });
+            } else {
+                tempEl.css({
+                    'top': i * spanHeight,
+                    border: options.margin + 'px solid ' + options.color,
+                    borderRight: borders.borderWidthTop + 'px solid ' + options.color,
+                    borderLeft: borders.borderWidthBottom + 'px solid ' + options.color,
+                    'height': spanHeight,
+                    'width': options.width
+                });
+            }
+
+            $el.prepend(tempEl);
+        };
+
+        blinds = $('span', el);
+        $el.delay(options.slideVisibleTime).queue(function(next) {
+            animateBorders();
+            next();
+        });
+    };
+
+    function calculateBorders() {
+        var random = Math.floor((Math.random()*9)+1);
+        var borderWidthTop = (random / 10) * options.gap;
+        var borderWidthBottom = options.gap - borderWidthTop;
+
+        return {borderWidthTop: borderWidthTop, borderWidthBottom: borderWidthBottom};
+    }
+
+    function animateBorders() {
+        var changeOccuredOnce = false;
+        var animationProperties = null
+
+        if(options.orientation === 'vertical') {
+            animationProperties = {
+                borderTopWidth: options.height / 2,
+                borderBottomWidth: options.height / 2
+            };
+        } else {
+            animationProperties = {
+                borderRightWidth: options.width / 2,
+                borderLeftWidth: options.width / 2
+            };
+        }
+
+        blinds.animate(animationProperties, options.animationSpeed, function() {
+            if(!changeOccuredOnce) {
+                images[activeImage].removeClass('active');
+                activeImage = (activeImage + 1) % images.length;
+                images[activeImage].addClass('active');
+                if(options.hasLinks) {
+                    el.href = urls[activeImage];
+                }
+                setTimeout(animateBorders, options.slideVisibleTime + options.animationSpeed);
+                changeOccuredOnce = true;
+            }
+
+            var borders = calculateBorders();
+
+            if(options.orientation === 'vertical') {
+                animationProperties = {
+                    borderTopWidth: borders.borderWidthTop,
+                    borderBottomWidth: borders.borderWidthBottom
+                };
+            } else {
+                animationProperties = {
+                    borderRightWidth: borders.borderWidthTop,
+                    borderLeftWidth: borders.borderWidthBottom
+                };
+            }
+
+            $(this).delay(options.delayBetweenSlides).animate(animationProperties, options.animationSpeed);
+        });
+    }
+
+    $.fn.blindify = function(userOptions) {
+        return this.each(function() {
+            new $.blindify(this, userOptions);
+        });
+    };
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,129 @@
+# Blindify
+
+Blindify is a **jQuery plugin** which creates a **slideshow** featuring a blinds effect transitioning (either vertical or horizontal) between a list of images. Have a look at the [demo](http://burnmind.com/demos/blindify).
+
+![blindify demo](https://raw.github.com/stathisg/blindify/master/demos/blindify-demo.gif)
+
+## Requirements
+
+jQuery 1.4+
+
+## Browser support
+
+*Tested* in the following browsers:
+
+* Google Chrome 29+
+* Mozilla Firefox 21+
+* Opera 12+
+* Microsoft Internet Explorer 9+
+* Safari 5+
+
+## Usage
+
+First of all, you have to include Blindify's CSS file (or just copy and paste the code to one of your own), include a version of the jQuery library, and anywhere after jQuery, either the full or the minified version of Blindify. For example:
+
+```html
+<link rel="stylesheet" href="blindify.css" media="all" />
+<script type="text/javascript" src="jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="jquery.blindify.js"></script>
+```
+
+Afterwards, you'll need a list of images, wrapped in a container element with a unique id. For example:
+
+```html
+<div id="blindify">
+    <ul>
+        <li><img src="photo_1.jpg" alt="" /></li>
+        <li><img src="photo_2.jpg" alt="" /></li>
+        <li><img src="photo_3.jpg" alt="" /></li>
+        <li><img src="photo_4.jpg" alt="" /></li>
+    </ul>
+</div>
+```
+
+You can make the whole slideshow be a link to a specific page, by using an anchor element as the container, e.g.:
+
+```html
+<a href="#" id="blindify">
+    <ul>
+        <li><img src="photo_1.jpg" alt="" /></li>
+        <li><img src="photo_2.jpg" alt="" /></li>
+        <li><img src="photo_3.jpg" alt="" /></li>
+        <li><img src="photo_4.jpg" alt="" /></li>
+    </ul>
+</a>
+```
+
+If you wish every image to point to a unique URL, you just have to specify an option while initialising the plugin, and format your code as follows:
+
+```html
+<div id="blindify">
+    <ul>
+        <li><a href="#"><img src="photo_1.jpg" alt="" /></a></li>
+        <li><a href="#"><img src="photo_2.jpg" alt="" /></a></li>
+        <li><a href="#"><img src="photo_3.jpg" alt="" /></a></li>
+        <li><a href="#"><img src="photo_4.jpg" alt="" /></a></li>
+    </ul>
+</div>
+```
+
+Finally, to apply Blindify to your HTML code, you'll just need to initialise it and attach it to the element acting as the container, e.g.:
+
+```html
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#blindify').blindify();
+    });
+</script>
+```
+
+You can override the default options of the plugin, like that:
+
+```html
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#blindify').blindify({
+            numberOfBlinds: 10,
+            animationSpeed: 600,
+            delayBetweenSlides: 200
+        });
+    });
+</script>
+```
+
+You can find a list of the available options in the next section.
+
+# Configuration Options
+
+`numberOfBlinds` (integer) — *default: 20*  
+The total number of blinds.
+
+`slideVisibleTime` (integer) — *default: 2000*  
+Controls the time that a slide will be visible (excluding the animation time), in milliseconds (ms).
+
+`color` (string) — *default: '#000000'*  
+The HEX code of the colour of the blinds.
+
+`margin` (integer) — *default: 2*  
+Controls the distance between the blinds, in pixels. Note that it's the border size of each individual blind, therefore if you choose 1, the total distance is going to be 2 (one for each blind), etc.
+
+`width` (integer) — *default: 960*  
+The width of the container (should be the same as the images), in pixels.
+
+`height` (integer) — *default: 600*  
+The height of the container (should be the same as the images), in pixels.
+
+`gap` (integer) — *default: 100*  
+The total gap of the slides from the edges of the container, in pixels. The individual gaps are calculated randomly.
+
+`animationSpeed` (integer) — *default: 100*  
+Controls the speed of the animation that opens/closes the blinds, in milliseconds (ms).
+
+`delayBetweenSlides` (integer) — *default: 500*  
+Controls a small delay between animating the blinds to their open state, in milliseconds (ms).
+
+`hasLinks` (boolean) — *default: false*  
+Set to `true`, when there are individual links for every image.
+
+`orientation` (string) — *default: 'vertical'*  
+The orientation of the blinds; it can be either `vertical` or `horizontal`.
